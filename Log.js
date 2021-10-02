8258d8fcb37970dbba5c37ce92f85ffd01aaada1
Suhaan2006 <81973250+Suhaan2006@users.noreply.github.com>
2021-10-02 20:57:13 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,14 @@
+class Log extends BaseClass {
+    constructor(x, y, height, angle) {
+      super(x,y,20,height,angle);
+
+      Matter.Body.setAngle(this.body, angle);
+      
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      super.display();
+    }
+  };
+  
